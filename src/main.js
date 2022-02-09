ba933bba0e9467804aba4788c4582bb456295b74
abualmun <muntherhafiz474@gmail.com>
2022-02-09 10:32:01 +0200

    register & edit

--- src/main.js
+++ src/main.js
@@ -20,6 +20,13 @@ db.run('CREATE TABLE users(username TEXT PRIMARY KEY,name TEXT,admin BOOL)', (er
         return console.log(err.message)
     console.log('users table created successfully')
 })
+//called when try to register
+app.post('/abualmun/register', function (req, res) {
+db.run(`INSERT INTO users (username,name,admin) VALUES (${data.username},${data.name},${data.admin})`, (err)=>{
+    if(err) return res.send(err)
+    res.send(success)
+})
+})
 
 //called when user tries to login.
 //received object keys:   [username]
@@ -44,27 +51,46 @@ app.post('/login', (req, res) => {
 
 
 })
-
+//called when requesting records
 app.post('/records', (req, res) => {
 
     data = req.body
-    db.all(`SELECT * FROM records WHERE username = ${data.username} AND date > ${data.start} AND date < ${data.end}`, (rows) => {
+    db.all(`SELECT * FROM records WHERE username = ${data.username} AND date > ${data.start} AND date < ${data.end} ORDER BY date ASC`, (rows) => {
         if (err) console.log(err)
         res.send(rows)
     })
 
 })
-
+//called when adding a new record
 app.post('/records/add', function (req, res) {
     data = req.body
     
     db.run(`INSERT INTO records(username,grade,sura,start, end,date) VALUES (${data.username},${data.grade},${data.sura},${data.start},${data.end},${data.date})`,(err)=>{
         if(err) return res.status(201).send(err)
         else {
-            res.send()
+            db.get(`SELECT * FROM records WHERE id = ${this.lastid}`,(row)=>{
+               res.send(row) 
+            })
         }
     } )
 
+})
+
+//called when editing record
+app.post('/records/edit', function (req, res) {
+    data = req.body
+    db.run(`UPDATE records SET username = ${data.username},grade = ${data.grade},sura = ${data.sura},start = ${data.start},end = ${data.end},date = ${data.date} WHERE id = ${data.id}`,(row)=>{
+        res.send(row)
+    })
+})
+
+
+//called when deleting record
+app.post('/records/delete', function (req, res) {
+    data = req.body
+    db.run(`DELETE FFROM records WHERE id = ${data.id}`, (err) => {
+        res.send('success')
+    })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
