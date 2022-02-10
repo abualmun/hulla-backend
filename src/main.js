b9ac6410f086c1af8f268dd02473314d88ceb425
abualmun <muntherhafiz474@gmail.com>
2022-02-10 13:49:30 +0200

    fixing all bugs

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const sqlite3 = require('sqlite3')
 app.use(express.json())
 const port = process.env.PORT || 5000
 
-let db = sqlite3.Database('./db/mainDatabase.db', (err) => {
+let db = new sqlite3.Database('./db/mainDatabase.db', (err) => {
     if (err) return console.log(err.message)
     console.log('database created')
 })
@@ -22,10 +22,11 @@ db.run('CREATE TABLE users(username TEXT PRIMARY KEY,name TEXT,admin BOOL)', (er
 })
 //called when try to register
 app.post('/abualmun/register', function (req, res) {
-db.run(`INSERT INTO users (username,name,admin) VALUES (${data.username},${data.name},${data.admin})`, (err)=>{
-    if(err) return res.send(err)
-    res.send(success)
-})
+    const data = req.body
+    db.run(`INSERT INTO users(username,name,admin) VALUES (?,?,?)`, [data.username, data.name, data.admin], (err) => {
+        if (err) return console.log(err)
+        res.send('success')
+    })
 })
 
 //called when user tries to login.
@@ -34,13 +35,14 @@ db.run(`INSERT INTO users (username,name,admin) VALUES (${data.username},${data.
 app.post('/login', (req, res) => {
     const username = req.body.username
 
-    db.get(`SELECT * FROM users WHERE username = ${username}`, (row) => {
+    db.get(`SELECT * FROM users WHERE username = ?`, [username], (err, row) => {
         if (err) console.log(err)
         if (!row) return res.status(201).send("can't find user with that username.")
-        if (!row.admin) return res.send({ 'name': row.name, 'admin': 0, 'studentlist': [{ 'name': row.name, 'username': row.username }] })
-        else {
+        if (!row.admin) {
+            return res.send({ 'name': row.name, 'admin': 0, 'studentlist': [{ 'name': row.name, 'username': row.username }] })
+        } else {
 
-            db.all('SELECT (name,username) FROM users WHERE admin=0', (rows) => {
+            db.all('SELECT * FROM users WHERE admin=0', (err, rows) => {
                 if (err) console.log(err)
                 return res.send({ 'name': row.name, 'admin': 1, 'studentlist': rows })
             })
@@ -48,14 +50,12 @@ app.post('/login', (req, res) => {
 
 
     })
-
-
 })
 //called when requesting records
 app.post('/records', (req, res) => {
 
     data = req.body
-    db.all(`SELECT * FROM records WHERE username = ${data.username} AND date > ${data.start} AND date < ${data.end} ORDER BY date(date) ASC`, (rows) => {
+    db.all(`SELECT * FROM records WHERE username = ? AND date >= ? AND date <= ? ORDER BY date ASC`, [data.username, data.start, data.end], (err, rows) => {
         if (err) console.log(err)
         res.send(rows)
     })
@@ -64,23 +64,27 @@ app.post('/records', (req, res) => {
 //called when adding a new record
 app.post('/records/add', function (req, res) {
     data = req.body
-    
-    db.run(`INSERT INTO records(username,grade,sura,start, end,date) VALUES (${data.username},${data.grade},${data.sura},${data.start},${data.end},${data.date})`,(err)=>{
-        if(err) return res.status(201).send(err)
+
+    db.run(`INSERT INTO records(username,grade,sura,start,end,date) VALUES (?,?,?,?,?,?)`, [data.username, data.grade, data.sura, data.start, data.end, data.date], function (err) {
+        if (err) return res.status(201).send(err.message)
         else {
-            db.get(`SELECT * FROM records WHERE id = ${this.lastid}`,(row)=>{
-               res.send(row) 
+            console.log(this.lastID)
+            db.get(`SELECT * FROM records WHERE id = ? `, [this.lastID], (err, row) => {
+                if (err) console.log(err)
+                res.send(row)
             })
         }
-    } )
+    })
 
 })
 
 //called when editing record
 app.post('/records/edit', function (req, res) {
     data = req.body
-    db.run(`UPDATE records SET username = ${data.username},grade = ${data.grade},sura = ${data.sura},start = ${data.start},end = ${data.end},date = ${data.date} WHERE id = ${data.id}`,(row)=>{
-        db.get(`SELECT * FROM records WHERE id = ${req.body.id}`,(row)=>{
+    db.run(`UPDATE records SET username = ?,grade = ?,sura = ?,start = ?,end = ?,date = ? WHERE id = ?`, [data.username, data.grade, data.sura, data.start, data.end, data.date, data.id], (err, row) => {
+        if (err) return res.send(err)
+        db.get(`SELECT * FROM records WHERE id = ${req.body.id}`, (err, row) => {
+            if (err) res.send(err)
             res.send(row)
         })
     })
@@ -90,7 +94,8 @@ app.post('/records/edit', function (req, res) {
 //called when deleting record
 app.post('/records/delete', function (req, res) {
     data = req.body
-    db.run(`DELETE FFROM records WHERE id = ${data.id}`, (err) => {
+    db.run(`DELETE FROM records WHERE id = ${data.id}`, (err) => {
+        if (err) return res.send(err)
         res.send('success')
     })
 })
